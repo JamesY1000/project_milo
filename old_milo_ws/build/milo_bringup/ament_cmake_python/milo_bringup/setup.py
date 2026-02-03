a057d6c162a7831bbfb2671458cad247bf9fcedd
James Yuan <jamesyuan1000@gmail.com>
2026-02-04 06:26:50 +1100

    Removed install build log directories

--- old_milo_ws/build/milo_bringup/ament_cmake_python/milo_bringup/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='milo_bringup',
-    version='0.0.0',
-    packages=find_packages(
-        include=('milo_bringup', 'milo_bringup.*')),
-)
--- old_milo_ws/build/milo_description/ament_cmake_python/milo_description/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='milo_description',
-    version='0.0.0',
-    packages=find_packages(
-        include=('milo_description', 'milo_description.*')),
-)
