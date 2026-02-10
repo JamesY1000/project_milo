7e3c63a9a155af40c54ce5e0281bd2a9aa6392bf
James Yuan <jamesyuan1000@gmail.com>
2026-02-10 11:27:59 +1100

    test serial receive

--- milo_teensy/src/read_serial.cpp
+++ milo_teensy/src/read_serial.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
 
-
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("USB serial logger ready");
-
+  while (!Serial) ; // Wait for Serial to be ready (optional, but useful)
+  Serial.println("Teensy USB serial debug ready");
 }
 
 void loop()
 {
-
+  if (Serial.available())
+  {
+    String data = Serial.readStringUntil('\n');
+    Serial.print("Received: ");
+    Serial.println(data);
+  }
 }
